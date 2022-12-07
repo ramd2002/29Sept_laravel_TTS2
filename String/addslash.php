c47f1b6db3ee1d7d41b2ec9c8f6d67a792db7a71
ramd2002 <114863759+ramd2002@users.noreply.github.com>
2022-12-07 10:57:40 +0530

    Add files via upload

---
+++ String/addslash.php
@@ -0,0 +1,11 @@
+<?php
+
+
+//echo "select * from users where text ='Ram\'s no'";
+$s = "Welcome to 't TOPS' Tech'no";
+echo $s = "this is Ram\'s no";
+echo "<br>";
+echo $s =  addslashes("this is Ram's no");
+
+
+?>
---
+++ String/intro.php
@@ -0,0 +1,27 @@
+<?php
+
+$str ="Something 123";
+$str1 ='Something#$%35 ';
+
+echo $str1;
+echo "<br>";
+echo $str1[0];
+echo $str1[1];
+echo $str1[2];
+echo $str1[3];
+echo $str1[4];
+echo $str1[5];
+echo $str1[6];
+echo $str1[7];
+echo $str1[8];
+
+
+
+$name = "Test";
+echo "<br>";
+echo 'Name is $name in single qouate';
+echo "<br>";
+echo 'Name is '.$name.' in single qouate';
+echo "<br>";
+echo "Name is $name in double qouate";
+?>
